9b4416480d13ff1a958470ead4556631d967738f
Marek Gogoľ <info@marekgogol.sk>
2021-08-01 11:30:29 +0200

    added higher upload max filesize into php pool

--- src/Traits/PHPSettingsTrait.php
+++ src/Traits/PHPSettingsTrait.php
@@ -42,6 +42,8 @@ trait PHPSettingsTrait
         return [
             'error_log' => $this->getWebPath($domain, $config).'/'.(isset($config['www_path']) ? '' : 'logs/').'php.log',
             'memory_limit' => '256M',
+            'upload_max_filesize' => '40M',
+            'post_max_size' => '40M',
             'open_basedir' => $this->buildOpenBaseDirs($domain, $config),
             'disable_functions' => 'apache_child_terminate,apache_setenv,define_syslog_variables,escapeshellcmd,eval,exec,fp,fput,ftp_connect,ftp_exec,ftp_get,ftp_login,ftp_nb_fput,ftp_put,ftp_raw,ftp_rawlist,get_defined_functions,highlight_file,ini_alter,ini_get_all,ini_restore,inject_code,mysql_pconnect,openlog,passthru,pcntl_alarm,pcntl_async_signals,pcntl_exec,pcntl_fork,pcntl_get_last_error,pcntl_getpriority,pcntl_setpriority,pcntl_signal,pcntl_signal_dispatch,pcntl_signal_get_handler,pcntl_sigprocmask,pcntl_sigtimedwait,pcntl_sigwaitinfo,pcntl_strerror,pcntl_wait,pcntl_waitpid,pcntl_wexitstatus,pcntl_wifcontinued,pcntl_wifexited,pcntl_wifsignaled,pcntl_wifstopped,pcntl_wstopsig,pcntl_wtermsig,phpAds_XmlRpc,phpAds_remoteInfo,phpAds_xmlrpcDecode,phpAds_xmlrpcEncode,php_uname,popen,posix_getpwuid,posix_kill,posix_mkfifo,posix_setpgid,posix_setsid,posix_setuid,posix_uname,proc_nice,proc_terminate,shell_exec,show_source,symlink,syslog,system,xmlrpc_entity_decode',
         ];
