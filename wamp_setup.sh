10dccdedc1d2345055125f0b4cd6d0e9e023cc42
Marek Gogoľ <info@marekgogol.sk>
2022-05-22 15:39:28 +0200

    removed old installs

--- wamp_setup.sh
+++ wamp_setup.sh
@@ -80,67 +80,6 @@ else
     fi
 fi
 
-# Check if php-cli is installed
-dpkg -s php7.1-cli &> /dev/null
-PHP71=$?
-if [ $PHP71 -eq 0 ]; then
-    echo -e "\e[32mPHP 7.1 version installed.\e[0m"
-else
-    read -p 'Do you want to install PHP 7.1? [Y/n]:' answer
-    answer=${answer:Y}
-
-    if [[ $answer =~ [Yy] ]]; then
-        add_ppa_if_not_exists ondrej/php
-        apt install -y php7.1-fpm && apt install -y php7.1-cli php7.1-fpm php7.1-soap php7.1-pdo php7.1-mysql php7.1-zip php7.1-gd php7.1-mbstring php7.1-curl php7.1-xml php7.1-bcmath php7.1-json php7.1-redis php7.1-iconv php7.1-imagick php7.1-intl
-        service php7.1-fpm start
-    fi
-fi
-
-dpkg -s php7.2-cli &> /dev/null
-PHP72=$?
-if [ $PHP72 -eq 0 ]; then
-    echo -e "\e[32mPHP 7.2 version installed.\e[0m"
-else
-    read -p 'Do you want to install PHP 7.2? [Y/n]:' answer
-    answer=${answer:Y}
-
-    if [[ $answer =~ [Yy] ]]; then
-        add_ppa_if_not_exists ondrej/php
-        apt install -y php7.2-fpm && apt install -y php7.2-cli php7.2-fpm php7.2-soap php7.2-pdo php7.2-mysql php7.2-zip php7.2-gd php7.2-mbstring php7.2-curl php7.2-xml php7.2-bcmath php7.2-json php7.2-redis php7.2-iconv php7.2-imagick php7.2-intl
-        service php7.2-fpm start
-    fi
-fi
-
-dpkg -s php7.3-cli &> /dev/null
-PHP73=$?
-if [ $PHP73 -eq 0 ]; then
-    echo -e "\e[32mPHP 7.3 version installed.\e[0m"
-else
-    read -p 'Do you want to install PHP 7.3? [Y/n]:' answer
-    answer=${answer:Y}
-
-    if [[ $answer =~ [Yy] ]]; then
-        add_ppa_if_not_exists ondrej/php
-        apt install -y php7.3-fpm && apt install -y php7.3-cli php7.3-fpm php7.3-soap php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json php7.3-redis php7.3-iconv php7.3-imagick php7.3-intl
-        service php7.3-fpm start
-    fi
-fi
-
-dpkg -s php7.4-cli &> /dev/null
-PHP74=$?
-if [ $PHP74 -eq 0 ]; then
-    echo -e "\e[32mPHP 7.4 version installed.\e[0m"
-else
-    read -p 'Do you want to install PHP 7.4? [Y/n]:' answer
-    answer=${answer:Y}
-
-    if [[ $answer =~ [Yy] ]]; then
-        add_ppa_if_not_exists ondrej/php
-        apt install -y php7.4-fpm && apt install -y php7.4-cli php7.4-fpm php7.4-soap php7.4-pdo php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-json php7.4-redis php7.4-iconv php7.4-imagick php7.4-intl
-        service php7.4-fpm start
-    fi
-fi
-
 dpkg -s php8.0-cli &> /dev/null
 PHP80=$?
 if [ $PHP80 -eq 0 ]; then
@@ -203,22 +142,6 @@ else
         rm mysql-apt-config_0.8.15-1_all.deb
         apt-get update
 
-        apt install mysql-server
-        service mysql start
-        mysql_secure_installation
-    fi
-fi
-
-# Check if mysql is installed
-dpkg -s mysql-server &> /dev/null
-IS_MYSQL=$?
-if [ $IS_MYSQL -eq 0 ]; then
-    echo -e "\e[32mMySQL is installed\e[0m"
-else
-    read -p 'Do you want to install MySQL 5.7? [Y/n]:' answer
-    answer=${answer:Y}
-
-    if [[ $answer =~ [Yy] ]]; then
         apt install mysql-server
         service mysql start
         mysql_secure_installation
