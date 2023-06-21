e741ec4b681c99428a8432ed320bf22e290da228
Marek Gogoľ <info@marekgogol.sk>
2023-06-21 12:00:07 +0200

    fixes

--- wamp_setup.sh
+++ wamp_setup.sh
@@ -90,7 +90,7 @@ else
 
     if [[ $answer =~ [Yy] ]]; then
         add_ppa_if_not_exists ondrej/php
-        apt install -y php8.0-fpm && apt install -y php8.0-cli php8.0-fpm php8.0-soap php8.0-mysql php8.0-zip php8.0-gd php8.0-mbstring php8.0-curl php8.0-xml php8.0-bcmath php8.0-redis php8.0-common php8.0-imagick php8.0-intl
+        apt install -y php8.0-fpm && apt install -y php8.0-cli php8.0-fpm php8.0-soap php8.0-mysql php8.0-zip php8.0-gd php8.0-mbstring php8.0-curl php8.0-xml php8.0-bcmath php8.0-redis php8.0-common php8.0-imagick php8.0-intl php8.0-tidy
         service php8.0-fpm start
     fi
 fi
@@ -105,7 +105,7 @@ else
 
     if [[ $answer =~ [Yy] ]]; then
         add_ppa_if_not_exists ondrej/php
-        apt install -y php8.2-fpm && apt install -y php8.2-cli php8.2-fpm php8.2-soap php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath php8.2-redis php8.2-common php8.2-imagick php8.2-intl
+        apt install -y php8.2-fpm && apt install -y php8.2-cli php8.2-fpm php8.2-soap php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath php8.2-redis php8.2-common php8.2-imagick php8.2-intl php8.2-tidy
         service php8.2-fpm start
     fi
 fi
